f57b11b24653b411c056601e38dfba6e29622b35
Yordy <iYord@protonmail.com>
2024-04-16 20:53:33 -0400

    .

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require("path");
 
 module.exports = {
   output: {
-    filename: "/src/index.html",
-    path: path.resolve("/src/index.html", "dist"),
+    filename: "index.html",
+    path: path.resolve("/src", "dist"),
   },
 };
